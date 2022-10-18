c51823dab7352af6df9102d14c05bdfde3784c07
Pol Moreno Castelar <pmorenoc@edu.tecnocampus.cat>
2022-10-18 17:01:05 +0200

    .

---
+++ Practica2_PolMoreno_GerardPiquer/Practica2_PolMoreno_GerardPiquer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practica2_PolMoreno_GerardPiquer
+//
+//  Created by Pol Moreno Castelar on 18/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
