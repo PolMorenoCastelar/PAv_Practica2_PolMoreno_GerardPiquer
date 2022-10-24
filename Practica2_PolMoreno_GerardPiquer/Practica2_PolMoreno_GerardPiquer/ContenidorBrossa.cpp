b31a7759542e91c858ae557114fee0bb7a9e0d90
GerardPiquerArrebola <gpiquer@edu.tecnocampus.cat>
2022-10-24 15:40:51 +0200

    noves modificacions classe ContenidorBrossa

--- Practica2_PolMoreno_GerardPiquer/Practica2_PolMoreno_GerardPiquer/ContenidorBrossa.cpp
+++ Practica2_PolMoreno_GerardPiquer/Practica2_PolMoreno_GerardPiquer/ContenidorBrossa.cpp
@@ -6,3 +6,20 @@
 //
 
 #include "ContenidorBrossa.hpp"
+
+ContenidorBrossa::ContenidorBrossa(std::string codi, int color, std::string ubicacio, int anyColocacio, float tara) {
+    //fer un metode privat check per comprovar si el codi es correcte
+    if () {
+        throw "Codi amb valor null";
+    }
+    if (color != GROC && color != MARRO && color != VERD && color != GRIS && color != BLAU) {
+        throw "Color de contenidor no valid, ha de ser de color groc, marro, verd, gris o blau";
+    }
+    //fer un metode private check per comprovar si l'any de colocacio es correcte
+    if () {
+        throw "L'any de colocacio ha de ser un enter de 4 xifres";
+    }
+    if (tara <= 0) {
+        throw "Valor de tara inferior o igual a 0";
+    }
+}
--- Practica2_PolMoreno_GerardPiquer/Practica2_PolMoreno_GerardPiquer/ContenidorBrossa.hpp
+++ Practica2_PolMoreno_GerardPiquer/Practica2_PolMoreno_GerardPiquer/ContenidorBrossa.hpp
@@ -9,5 +9,37 @@
 #define ContenidorBrossa_hpp
 
 #include <stdio.h>
+#include <string>
+
+class ContenidorBrossa {
+public:
+    static const int GROC = 1, MARRO = 2, VERD = 3, GRIS = 4, BLAU = 5;
+    ContenidorBrossa(std::string codi, int color, std::string ubicacio, int anyColocacio, float tara);
+    ContenidorBrossa(std::string codi, int color, int anyColocacio, int anyRetirada, float tara);
+    std::string getTipusBrossa();
+    void retirarViaPublica();
+    std::string getUbicacio();
+    void setUbicacio(std::string ubicacio);
+    std::string getCodi();
+    std::string getEstat();
+    bool operator==(ContenidorBrossa *p);
+    bool operator<(ContenidorBrossa *p);
+    bool operator>(ContenidorBrossa *p);
+    void toString();
+    void buidat(float pes);
+    std::string getType();
+    std::string getReciclat();
+    ~ContenidorBrossa();
+    
+private:
+    std::string codi;
+    int color;
+    std::string ubicacio;
+    int anyColocacio;
+    int anyRetirada;
+
+protected:
+    int tara;
+};
 
 #endif /* ContenidorBrossa_hpp */
