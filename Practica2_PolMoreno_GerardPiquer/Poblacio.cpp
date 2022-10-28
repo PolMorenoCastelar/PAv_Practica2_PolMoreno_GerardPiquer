88c3b53d9fcc4fc2b0619aff66d3e4ecb7778b11
Pol Moreno Castelar <pmorenoc@edu.tecnocampus.cat>
2022-10-28 17:41:28 +0200

    .

--- Practica2_PolMoreno_GerardPiquer/Poblacio.cpp
+++ Practica2_PolMoreno_GerardPiquer/Poblacio.cpp
@@ -6,3 +6,50 @@
 //
 
 #include "Poblacio.hpp"
+#include <iostream>
+#include <time.h>
+#include <string.h>
+
+Poblacio::Poblacio(){
+    contenidor = new node*[1];
+}
+Poblacio::Poblacio(ContenidorBrossa *c){
+    Poblacio();
+    afegirContenidor(c);
+}
+void Poblacio::afegirContenidor(ContenidorBrossa *p){
+    if(hiEsContenidor(p, *contenidor)){
+        throw ("Aquest contenidor ja es troba al magatzem!");
+    }
+    
+    node* aux= new node();
+    aux->con=p;
+    aux->seg=*contenidor;
+    *contenidor = aux;
+}
+
+bool Poblacio::hiEsContenidor(ContenidorBrossa *c, node *contenidor){
+           while(contenidor!=nullptr){
+               if(*c == contenidor->con){
+                   return true;
+               }
+               contenidor=contenidor->seg;
+           }
+           return false;
+}
+void Poblacio::afegirContenidor(std::string codi, int color, std::string ubicacio, int anyColocacio, float tara){
+    afegirContenidor(new ContenidorBrossa(codi,color,anyColocacio,tara));
+}
+std::string Poblacio::hiEs(std::string codi){
+    node *x =*contenidor;
+    while(x!=nullptr){
+        if(x->con->getCodi()== codi){
+            return "El contenidor amb codi"+codi+" es de color "+x->con->getColor();
+        }
+       x=x->seg;
+    }
+    throw ("No es troba a la població");
+}
+
+           
+
--- Practica2_PolMoreno_GerardPiquer/Poblacio.hpp
+++ Practica2_PolMoreno_GerardPiquer/Poblacio.hpp
@@ -13,12 +13,28 @@
 
 class Poblacio {
 public:
-
+    Poblacio();
+    Poblacio(ContenidorBrossa* c);
+    void afegirContenidor(ContenidorBrossa *g);
+    void afegirContenidor(std::string codi, int color, std::string ubicacio,int anyColocacio,float tara);
+    std::string hiEs(std::string codi);
+    void eliminarContenidor(ContenidorBrossa *c);
+    ContenidorBrossa* mesRendiment();
+    int getQuants(int color);
+    int getQuants();
+    bool operator==(Poblacio d);
+    bool operator<(Poblacio d);
+    bool operator>(Poblacio d);
+    void toString();
+    
 private:
+    
     struct node {
         ContenidorBrossa *con;
         node* seg;
     };
+    node** contenidor;
+    bool hiEsContenidor(ContenidorBrossa *c,node *contenidor);
 };
 
 #endif /* Poblacio_hpp */
